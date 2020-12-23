fafb88dfc241e08fd7427b39e03bab9471fe5cbe
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-12-23 15:41:27 +0100

    Modif

--- script.js
+++ script.js
@@ -1,29 +1,61 @@
-let date = new Date();
-
-document.getElementById("date").innerHTML = "Date du jour: " + date.getDate() + "/" + (date.getMonth() +1) + "/" + date.getFullYear();
+const date = new Date();
 
 let hours = date.getHours();
 let minutes = date.getMinutes();
 let seconds = date.getSeconds();
+let day = date.getDate();
+let month = (date.getMonth() +1);
+let year = date.getFullYear();
+let hour = document.getElementById("hours");
+let minute = document.getElementById("minutes");
+let second = document.getElementById("seconds");
+
+document.getElementById("date").innerHTML = "Date du jour: " + day + "/" + month + "/" + year;
+
+sec();
 
 function sec() {
-    setTimeout(function () {
-        document.getElementById("seconds").style.transform = "rotate(" + ((seconds + 45) * 6) +"deg)";
-        document.getElementById("minutes").style.transform = "rotate(" + ((minutes + 45) * 6) +"deg)";
-        document.getElementById("hours").style.transform = "rotate(" + ((hours + 45) * 30) +"deg)";
-        console.log(minutes);
-        seconds++
-        if(seconds === 60) {
-            seconds = 0;
-            minutes++;
-        }
-        if(minutes === 60) {
+    second.style.transform = "rotate(" + ((seconds + 45) * 6) +"deg)";
+    minute.style.transform = "rotate(" + ((minutes + 45) * 6) +"deg)";
+    hour.style.transform = "rotate(" + ((hours + 45) * 30) +"deg)";
+    seconds++
+    if(seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if(minutes >= 60) {
             minutes = 0;
-            hours++
+            hours++;
+            if(hours >= 24) {
+                hours = 0;
+                day++;
+                if(month === 2) {
+                    if(year % 4 === 0) {
+                        if(day >= 29){
+                            day = 1;
+                            month++
+                        }
+                    }
+                    else if(day >= 28) {
+                        day = 1;
+                        month++
+                    }
+                }
+                else if(month === (1 || 3 || 5 || 7 || 8 || 10 || 12)) {
+                    if(day >= 31) {
+                        day = 1;
+                        month++
+                    }
+                }
+                else if(day >= 30) {
+                    day = 1;
+                    month++
+                }
+                if(month >= 12) {
+                    month = 1;
+                    year++
+                }
+            }
         }
-        sec()
-    }, 1000)
+    }
+    setTimeout( sec, 1000)
 }
-
-
-sec()
